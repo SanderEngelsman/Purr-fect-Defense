010af5ffd741e36bd5459ba4317ea2d3cc33a36e
SanderEngelsman <sanderplaysde@gmail.com>
2025-05-21 11:41:30 +0200

    Enemy & Tower Scripts

--- Purr-fect Defense/Assets/Sander/Scripts/Data/TowerData.cs
+++ Purr-fect Defense/Assets/Sander/Scripts/Data/TowerData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "New Tower", menuName = "Tower Defense/Tower")]
-public class TowerData : MonoBehaviour
+public class TowerData : ScriptableObject
 {
     public GameObject prefab;
     public float cost;
