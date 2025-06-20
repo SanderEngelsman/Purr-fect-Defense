5a81eeba2267ec322f625b0ad7bb0c04e6fb5a90
Elv3Nstar <97114563@st.deltion.nl>
2025-06-20 08:58:21 +0200

    fixed tutorial video

--- Purr-fect Defense/Assets/Sander/Scripts/UI/Menu.cs
+++ Purr-fect Defense/Assets/Sander/Scripts/UI/Menu.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
 
 public class Menu : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
+
+    public void Start()
+    {
+        if (videoPlayer == null)
+        {
+
+        }
+    }
+
     public void Freeze()
     {
         Time.timeScale = 0f;
@@ -25,5 +37,15 @@ public class Menu : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartVideo()
+    {
+        videoPlayer.Play();
+    }
+
+    public void StopVideo()
+    {
+        videoPlayer.Stop();
     }
 }
